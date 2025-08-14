d0f80f1932b83c9b4b10e62653a3a5d62360a261
IsikYildiz <isik.yildizwork@gmail.com>
2025-08-14 18:13:54 +0300

    Deleted unnecessary files

--- backend/temp/user_code.py
+++
@@ -1,8 +0,0 @@
-def permutation(sequence: list[int | str | float]) -> list[list[int | str | float]]:
-    if len(sequence) == 1:
-        return [sequence]
-    result: list[list[int | str | float]] = []
-    for i in range(len(sequence)):
-        for perm in permutation(sequence[:i] + sequence[i + 1 :]):
-            result.append([sequence[i]] + perm)
-    return result
